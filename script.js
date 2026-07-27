1991686c7b03ff485bb2e42c80066bab8aaa828d
zaborchiki <lalagoga34@gmail.com>
2026-07-27 23:19:55 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE: "http://192.168.1.36:3001/api",
+  API_BASE: "http://100.126.25.87:3001/api",
   LOCATION_FALLBACK: "SANT'ANGELO A CUPOLO",
   // Coordinates of Sant'Angelo a Cupolo, Benevento, Campania, Italy.
   LAT: 41.0691,
